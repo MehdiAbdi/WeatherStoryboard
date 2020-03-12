c23f6fb9f46fa7028de66edf21fd5314f0bdc828
Mehdi Abdi <Appleseed.me77@gmail.com>
2020-03-12 15:04:03 +0330

    *Configure Location

--- WeatherStoryboard/Controller/ViewController.swift
+++ WeatherStoryboard/Controller/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageCondition: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    fileprivate let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
     }
-
-
 }
 
+// MARK: - CoreLocation Extension
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let latitude  = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            print(latitude,longitude)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
